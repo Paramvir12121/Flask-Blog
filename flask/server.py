95d657cf25f9d029671ca4413e28640dc38e5217
paramvir12121 <paramvir12121@gmail.com>
2024-03-12 16:50:55 -0400

    working navbar

--- flask/server.py
+++ flask/server.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html",current_year=current_year)
+    return render_template("home.html",current_year=current_year)
 
 @app.route("/about")
 def about():
