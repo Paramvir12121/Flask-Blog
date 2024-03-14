dd5befa15d945a3eae400e250cecf8a095238190
Paramvir Singh <paramvir12121@gmail.com>
2024-03-14 10:33:48 +0000

    login page created

--- flask/server.py
+++ flask/server.py
@@ -29,6 +29,16 @@ def contact():
     else:
         return render_template("contact.html",current_year=current_year)
 
+
+@app.route("/login",methods=[ "GET","POST"])
+def login():
+    return render_template("login.html",current_year=current_year)
+
+
+
+
+
+
 # to run whithout using cmd >flask --app <filename> run 
 # Allows to just use python hello.py
 if __name__ == "__main__":
